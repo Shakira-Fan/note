35dcf9b0fcf711d912d2f8a0fe8d77b33acfe70e
ShakiraFan <linda40685@gmail.com>
2021-11-01 16:37:28 +0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import GlobalStyle from './components/GlobalStyle';
 import Pages from './Pages';
 
 function App() {
   return (
     <div>
+      <GlobalStyle />
       <Pages />
     </div>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,21 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+  padding: 1em;
+  background: #f5f4f0;
+
+  @media (max-width: 700px) {
+    padding-top: 64px;
+  }
+
+  @media (min-width: 700px) {
+    position: fixed;
+    width: 220px;
+    height: calc(100% - 64px);
+    overflow-y: scroll;
+  }
+`;
+
+const NavList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  line-height: 2;
+
+  /* We can nest styles in styled-components */
+  /* The following styles will apply to links within the NavList component */
+  a {
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 1.1em;
+    color: #333;
+  }
+
+  a:visited {
+    color: #333;
+  }
+
+  a:hover,
+  a:focus {
+    color: #0077cc;
+  }
+`;
 
 const Navigation = () => {
     return (
-        <nav>
-            <ul>
+        <Nav>
+            <NavList>
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link to="/">
+                        <span aria-hidden="true" role="img">
+                            🏠
+                        </span>
+                        Home</Link>
                 </li>
                 <li>
-                    <Link to="/mynotes">My Notes</Link>
+                    <Link to="/mynotes">
+                        <span aria-hidden="true" role="img">
+                            📓
+                        </span>
+                        My Notes</Link>
                 </li>
                 <li>
-                    <Link to="/favorites">Favorites</Link>
+                    <Link to="/favorites">
+                        <span aria-hidden="true" role="img">
+                            🌟
+                        </span>
+                        Favorites</Link>
                 </li>
-            </ul>
-        </nav>
+            </NavList>
+        </Nav>
     );
 }
 
